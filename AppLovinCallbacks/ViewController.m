a5cec99afc9945c1a7bc3f224a43acd6f280b23b
Maximilian Tagher <feedback.tagher@gmail.com>
2015-06-29 13:43:39 -0700

    Initial Commit

---
+++ AppLovinCallbacks/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AppLovinCallbacks
+//
+//  Created by Maximilian Tagher on 6/29/15.
+//  Copyright (c) 2015 Heyzap. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
